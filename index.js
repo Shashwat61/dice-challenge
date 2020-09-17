4cc433e1768ba8943fab8d18fb794130acea3011
shashwat61 <shashwat742@gmail.com>
2020-09-17 12:28:09 +0530

    checkout dice with index

---
+++ index.js
@@ -0,0 +1,25 @@
+//for image1
+var randomnumber=Math.floor(Math.random() * 6)+1;
+var randomdice="dice"+randomnumber+".png";
+var randomimagesource="images/"+randomdice;
+var image=document.querySelectorAll("img")[0];
+image.setAttribute("src",randomimagesource);
+
+//for image2
+var randomnumber1=Math.floor(Math.random() * 6)+1;
+var randomdice1="dice"+randomnumber1+".png";
+var randomimagesource1="images/"+randomdice1;
+var image1=document.querySelectorAll("img")[1];
+image1.setAttribute("src",randomimagesource1);
+
+
+
+if(randomnumber>randomnumber1){
+  document.querySelector("h1").innerHTML="🎊Player 1 wins!";
+}
+else if(randomnumber<randomnumber1){
+  document.querySelector("h1").innerHTML="🎊Player 2 wins!";
+}
+else{
+  document.querySelector("h1").innerHTML="🎏It's a tie!";
+}
